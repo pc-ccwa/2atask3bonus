34c10d9ab0ad7d4440c297bd2c0813569d1d4370
pc-ccwa <115084257+pc-ccwa@users.noreply.github.com>
2022-10-20 07:56:35 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,21 @@
+let num = 0
+let count = 0
 input.onButtonPressed(Button.A, function () {
     num += 1
     basic.showNumber(num)
+    count = num
 })
 input.onButtonPressed(Button.AB, function () {
-    while (num >= 0) {
-        num += -1
-        basic.showNumber(num)
+    while (count > 0) {
+        count += -1
+        basic.showNumber(count)
     }
 })
 input.onButtonPressed(Button.B, function () {
     num += 10
     basic.showNumber(num)
+    count = num
 })
-let num = 0
-num = 0
 basic.forever(function () {
 	
 })
